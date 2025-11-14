d06778bd01767d7375dfa90f2e15cd4371644432
Darshan Nandagavi <nandagavidarshan562@gmail.com>
2025-11-14 22:42:01 +0530

    index.js file changed

--- server/routes/homeworkStatusRoutes.js
+++ server/routes/homeworkStatusRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getStudentHomeworkStatus, updateHomeworkStatus } from "../controllers/homeworkstatuscontroller";
+import { getStudentHomeworkStatus, updateHomeworkStatus } from "../controllers/homeworkstatuscontroller.js";
 
 
 
