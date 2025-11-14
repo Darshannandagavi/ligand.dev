f21459cace2121a1edf296263a18c4f7f11b52c3
Darshan Nandagavi <nandagavidarshan562@gmail.com>
2025-11-15 00:12:55 +0530

    production api added

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ import homeworkstatusRouter from "./routes/homeworkStatusRoutes.js";
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "https://ligand-dev-7.onrender.com",
+  origin: "https://developersligand.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
