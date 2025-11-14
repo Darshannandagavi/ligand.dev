435bf7ba9beb7fd9db8579c8a455c0a0e85603ea
Darshan Nandagavi <nandagavidarshan562@gmail.com>
2025-11-14 22:15:48 +0530

    import error solved

--- server/routes/homeworkStatusRoutes.js
+++ server/routes/homeworkStatusRoutes.js
@@ -3,20 +3,10 @@ import { getStudentHomeworkStatus, updateHomeworkStatus } from "../controllers/h
 
 
 
+
 const homeworkstatusRouter = express.Router();
 
 homeworkstatusRouter.post("/", updateHomeworkStatus);
 homeworkstatusRouter.get("/:studentId", getStudentHomeworkStatus);
-
-// ✅ Add this new route
-// homeworkstatusRouter.get("/unlocked-exams/:studentId", async (req, res) => {
-//   try {
-//     const { studentId } = req.params;
-//     const result = await getUnlockedExams(studentId);
-//     res.status(200).json(result);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
 
 export default homeworkstatusRouter;
