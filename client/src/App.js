ba9b064d57f1cdb6d4bc8641a0bf4980e5dc9efd
Darshan Nandagavi <nandagavidarshan562@gmail.com>
2025-11-16 10:00:12 +0530

    teacher forgot password route added in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -49,6 +49,7 @@ import TeacherRegister from "./components/TeacherLayout/TeacherRegister";
 import TeacherLogin from "./components/TeacherLayout/TeacherLogin";
 import Homework from "./components/AdminLayout/Homework";
 import TeacherHomework from "./components/TeacherLayout/TeacherHomeWork";
+import TeacherForgot from "./components/TeacherLayout/TeacherForgot";
 
 function App() {
   return (
@@ -59,27 +60,34 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/ForgotPassword" element={<ForgotPassword />} />
-          <Route path='/teacher-login' element={<TeacherLogin/>}/>
+          <Route path="/teacher-login" element={<TeacherLogin />} />
+          <Route path="teacher-forgot-pwd" element={<TeacherForgot />} />
         </Route>
 
         <Route path="/user" element={<UserLayout />}>
           <Route index element={<AttendExam />} />
-          <Route path="/user/notes" element={<Notes/>}/>
-          <Route path="/user/clientsetup" element={<MyNotes/>}/>
-          <Route path="/user/BootstrapinReact" element={<Chapter4/>}/>
-          <Route path="/user/CommonLayouts" element={<Chapter5/>}/>
-          <Route path="/user/LandingPages" element={<Chapter6/>}/>
-          <Route path="/user/IntroductiontoMERNStack" element={<Chapter3/>}/>
-          <Route path="/user/ServerSetup" element={<ServerSetup/>}/>
-          <Route path="/user/PostmanSetup" element={<PostmanSetup/>}/>
-          <Route path="/user/userbackend" element={<UserBackend/>}/>
-          <Route path="/user/RegistrationFrontend" element={<RegistrationFrontend/>}/>
-          <Route path="/user/loginfrontend" element={<LoginFrontend/>}/>
-          <Route path="/user/itembackend" element={<ItemBackend/>}/>
-          <Route path="/user/itemfrontend" element={<ItemFrontend/>}/>
-          <Route path="/user/rolebasednotes" element={<RoleBasedNotes/>}/>
-          <Route path="/user/viewitemfrontendnotes" element={<ViewItemFrontendNotes/>}/>
-          <Route path="/user/history" element={<ExamHistory/>}/>
+          <Route path="/user/notes" element={<Notes />} />
+          <Route path="/user/clientsetup" element={<MyNotes />} />
+          <Route path="/user/BootstrapinReact" element={<Chapter4 />} />
+          <Route path="/user/CommonLayouts" element={<Chapter5 />} />
+          <Route path="/user/LandingPages" element={<Chapter6 />} />
+          <Route path="/user/IntroductiontoMERNStack" element={<Chapter3 />} />
+          <Route path="/user/ServerSetup" element={<ServerSetup />} />
+          <Route path="/user/PostmanSetup" element={<PostmanSetup />} />
+          <Route path="/user/userbackend" element={<UserBackend />} />
+          <Route
+            path="/user/RegistrationFrontend"
+            element={<RegistrationFrontend />}
+          />
+          <Route path="/user/loginfrontend" element={<LoginFrontend />} />
+          <Route path="/user/itembackend" element={<ItemBackend />} />
+          <Route path="/user/itemfrontend" element={<ItemFrontend />} />
+          <Route path="/user/rolebasednotes" element={<RoleBasedNotes />} />
+          <Route
+            path="/user/viewitemfrontendnotes"
+            element={<ViewItemFrontendNotes />}
+          />
+          <Route path="/user/history" element={<ExamHistory />} />
           <Route path="/user/profile" element={<Profile />} />
           <Route path="/user/changepassword" element={<ChangePassword />} />
           <Route path="/user/interview" element={<InterviewPage />} />
@@ -90,32 +98,45 @@ function App() {
           <Route path="/admin/exam" element={<ManageExam />} />
           <Route path="/admin/notescontroll" element={<AdminNotesControl />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
-          <Route path="/admin/attendance" element={<AdminAttendance/>} />
-          <Route path="/admin/payments" element={<AdminPayments/>} />
-          <Route path="/admin/payments/mark" element={<AdminPaymentsMark/>} />
-          <Route path="/admin/payments/dashboard" element={<AdminPaymentsDashboard/>} />
-          <Route path="/admin/dashboard" element={<AdminDashboard/>} />
-          <Route path="/admin/admindashboard" element={<AdminNewDashboard/>} />
+          <Route path="/admin/attendance" element={<AdminAttendance />} />
+          <Route path="/admin/payments" element={<AdminPayments />} />
+          <Route path="/admin/payments/mark" element={<AdminPaymentsMark />} />
+          <Route
+            path="/admin/payments/dashboard"
+            element={<AdminPaymentsDashboard />}
+          />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/admindashboard" element={<AdminNewDashboard />} />
           <Route path="/admin/adminoptions" element={<AdminOptions />} />
-          <Route path="/admin/history" element={<AdminExamAttempts/>} />
+          <Route path="/admin/history" element={<AdminExamAttempts />} />
           <Route path="/admin/changepassword" element={<ChangePassword />} />
-          <Route path="/admin/manageexamsvisibility" element={<ManageExamVisibility />} />
-          <Route path="/admin/interviewcontrol" element={<InterviewControl/>} />
-          <Route path="/admin/add-teacher" element={<TeacherRegister/>}/>
-          <Route path="/admin/manage-teacher" element={<ManageTeachers/>}/>
-          <Route path="/admin/student/homework" element={<Homework/>}/>
+          <Route
+            path="/admin/manageexamsvisibility"
+            element={<ManageExamVisibility />}
+          />
+          <Route
+            path="/admin/interviewcontrol"
+            element={<InterviewControl />}
+          />
+          <Route path="/admin/add-teacher" element={<TeacherRegister />} />
+          <Route path="/admin/manage-teacher" element={<ManageTeachers />} />
+          <Route path="/admin/student/homework" element={<Homework />} />
         </Route>
 
         <Route path="/teacher" element={<TeacherLayout />}>
-          <Route path="/teacher/students" element={<TeacherStudent/>}/>
-          <Route index  element={<TeacherHomework/>}/>
-          <Route path="/teacher/homeworks" element={<TeacherHomework/>}/>
-          <Route path="/teacher/change-password" element={<TeacherChangePassword/>}/>
-          <Route path="/teacher/edit-profile" element={<TeacherEditProfile/>}/>
-          <Route path="/teacher/students" element={<TeacherStudent/>}/>
-          
+          <Route path="/teacher/students" element={<TeacherStudent />} />
+          <Route index element={<TeacherHomework />} />
+          <Route path="/teacher/homeworks" element={<TeacherHomework />} />
+          <Route
+            path="/teacher/change-password"
+            element={<TeacherChangePassword />}
+          />
+          <Route
+            path="/teacher/edit-profile"
+            element={<TeacherEditProfile />}
+          />
+          <Route path="/teacher/students" element={<TeacherStudent />} />
         </Route>
-      
       </Routes>
     </div>
   );
